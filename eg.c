9786de44a9bd9dc29ee39a1d688626159ff1acc9
Saran_8916 <saranteja8916@gmail.com>
2021-02-17 14:49:04 +0530

    voter eligibility program is created

---
+++ eg.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("Enter your age");
+	scanf("%d",&a);
+	if(a>=18)
+	{
+		printf("voter id is can be issued");
+	}
+	else
+	{
+		printf("voter id cannot be issued");
+	}
+}
